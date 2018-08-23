1b310d0b577f7aa129958f10611144e9a3dd6722
luigi.frasca@vigilfuoco.it <luigi.frasca@laz.dipvvf.it>
2018-08-23 16:28:46 +0200

    tutorial punto 7

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
-//app.use('/books', express.static(path.join(__dirname, 'dist')));
+app.use('/books', express.static(path.join(__dirname, 'dist/mean-angular5/')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-angular5/')));
 app.use('/book', book);
 
